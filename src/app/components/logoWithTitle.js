0817c4946716b880e233eb892f2d9f383602cc5c
felipegangorra <felipegangorra@gmail.com>
2025-02-11 22:22:22 -0300

    Fix the logo and add a README

--- src/app/components/logoWithTitle.js
+++ src/app/components/logoWithTitle.js
@@ -2,11 +2,11 @@ function LogoWithTitle() {
     return (
       <div className="flex items-center justify-center mb-8">
         <img
-          src="imgs/RPGo_logo.png" 
+          src="/img/rpgo-logo.png"
           alt="Logo"
-          className="w-12 h-12 mr-3"
+          className="w-28 h-28 ml-20"
         />
-        <h1 className="text-4xl font-bold text-white">RPGo</h1>
+        <h1 className="text-4xl font-bold text-white mr-40">RPGo!</h1>
       </div>
     );
   }
