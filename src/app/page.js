043b102a8b36d51b8801d397383b444e5f8e6eb7
sabrinabs <sabrina.silva@ccc.ufcg.edu.br>
2025-03-25 11:11:28 -0300

    redirecionamento profile page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,29 @@
 "use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import FormButton from "@/components/Forms/formButton";
 import LogoWithTitle from "@/components/Images/logoWithTitle";
-import Footer from "@/components/Footer/footer";
-import { useRouter } from "next/navigation";
 
 export default function HomePage() {
   const router = useRouter();
+
+  function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return decodeURIComponent(parts.pop().split(';').shift());
+    return null;
+  }
+
+  useEffect(() => {
+    const csrftoken = getCookie("csrftoken");
+
+    if (!csrftoken) {
+      router.push("/login");
+    } else {
+      router.push("/"); 
+    }
+  }, [router]);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-[calc(100vh-144px)] p-6">
       <div className="w-full max-w-sm">
